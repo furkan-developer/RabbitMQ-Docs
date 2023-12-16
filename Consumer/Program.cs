3aa767bc1c9b1967bf9be6af20ce32d859d01f08
furkan-developer <furknn2112@gmail.com>
2023-12-16 17:02:43 +0300

    Consumer project has been created

---
+++ Consumer/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args);
+
+using IHost host = builder.Build();
+
+// Application code should start here.
+
+await host.RunAsync();
