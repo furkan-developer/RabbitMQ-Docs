10f63eb2679ddeaa2dda499e83f00bec61209cc0
furkan-developer <furknn2112@gmail.com>
2023-12-16 17:31:21 +0300

    Consume ItemCreatedMessage

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,7 +1,26 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Consumer.Consumers;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Shared.Messages;
 
 IHostBuilder builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices(service =>
+{
+    service.AddMassTransit((config) =>
+    {
+        config.AddConsumer<ItemCreatedMessageConsume>();
+
+        config.UsingRabbitMq((ctx, cfg) =>
+        {
+            cfg.Host(host: "");
+
+            // ConfigureEndpoints should be the last method called after all settings and middleware components have been configured.
+            cfg.ConfigureEndpoints(ctx);
+        });
+    });
+});
 
 using IHost host = builder.Build();
 
